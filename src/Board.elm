845e71f919e4541a701f53de627d5b92a31335d9
Jon McAuliffe <jon.mcauliffe@tabcorp.com.au>
2019-11-29 09:36:29 +1100

    Refactor Board and Model out to modules

--- src/Board.elm
+++ src/Board.elm
@@ -1,6 +1,11 @@
-module Board exposing (Position, PostitionStatus(..))
+module Board exposing (Board, viewBoard)
 
 import Dict exposing (Dict)
+import Element exposing (Element, centerX, centerY, column, el, height, padding, px, rgb255, row, text, width)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
+import Msg exposing (Msg)
 
 
 type alias Position =
@@ -14,10 +19,77 @@ type GamePhase
     | GameOver
 
 
-type PostitionStatus
-    = Empty
-    | Mine
+viewBoard : Board -> Element Msg
+viewBoard board =
+    List.range 1 6
+        |> List.map (\r -> el [] <| viewRow r board)
+        |> column []
+
+
+viewRow : Int -> Board -> Element Msg
+viewRow r board =
+    List.range 1 6
+        |> List.map (\c -> viewSpace board ( r, c ))
+        |> row
+            (if board.sr == Just r then
+                [ Background.color (rgb255 200 200 200) ]
+
+             else
+                []
+            )
+
+
+viewSpace : Board -> ( Int, Int ) -> Element Msg
+viewSpace board ( r, c ) =
+    let
+        v =
+            Dict.get ( r, c ) board.playArea
+
+        w =
+            { bottom =
+                if r == 6 then
+                    4
+
+                else
+                    2
+            , left =
+                if c == 1 then
+                    4
+
+                else
+                    2
+            , right =
+                if c == 6 then
+                    4
+
+                else
+                    2
+            , top =
+                if r == 1 then
+                    4
+
+                else
+                    2
+            }
+    in
+    el [ width <| px 60, height <| px 60, Border.color <| rgb255 0 0 0, Border.widthEach w, padding 5 ] <|
+        case v of
+            Just a ->
+                text (String.fromInt a)
+
+            Nothing ->
+                text ""
+
+
+viewFace model =
+    text <| String.fromInt model.face
 
 
 type alias Board =
-    Dict Position PostitionStatus
+    { n : Maybe Int
+    , s : Maybe Int
+    , e : Maybe Int
+    , w : Maybe Int
+    , sr : Maybe Int
+    , playArea : Dict ( Int, Int ) Int
+    }
--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,6 @@
 module Main exposing (main)
 
+import Board exposing (..)
 import Browser
 import Dict exposing (Dict)
 import Element exposing (Element, centerX, centerY, column, el, height, padding, px, rgb255, row, text, width)
@@ -8,12 +9,8 @@ import Element.Border as Border
 import Element.Font as Font
 import Element.Input exposing (button)
 import Html exposing (Html)
+import Msg exposing (..)
 import Random
-
-
-type Msg
-    = ClickedRoll
-    | GotDiceIndex Int
 
 
 view : Model -> Html Msg
@@ -30,68 +27,6 @@ view model =
             ]
 
 
-viewBoard : Board -> Element Msg
-viewBoard board =
-    List.range 1 6
-        |> List.map (\r -> el [] <| viewRow r board)
-        |> column []
-
-
-viewRow : Int -> Board -> Element Msg
-viewRow r board =
-    List.range 1 6
-        |> List.map (\c -> viewSpace board ( r, c ))
-        |> row
-            (if board.sr == Just r then
-                [ Background.color (rgb255 200 200 200) ]
-
-             else
-                []
-            )
-
-
-viewSpace : Board -> ( Int, Int ) -> Element Msg
-viewSpace board ( r, c ) =
-    let
-        v =
-            Dict.get ( r, c ) board.playArea
-
-        w =
-            { bottom =
-                if r == 6 then
-                    4
-
-                else
-                    2
-            , left =
-                if c == 1 then
-                    4
-
-                else
-                    2
-            , right =
-                if c == 6 then
-                    4
-
-                else
-                    2
-            , top =
-                if r == 1 then
-                    4
-
-                else
-                    2
-            }
-    in
-    el [ width <| px 60, height <| px 60, Border.color <| rgb255 0 0 0, Border.widthEach w, padding 5 ] <|
-        case v of
-            Just a ->
-                text (String.fromInt a)
-
-            Nothing ->
-                text ""
-
-
 viewPanel model =
     column [ padding 60 ]
         [ button []
@@ -104,16 +39,6 @@ viewPanel model =
 
 viewFace model =
     text <| String.fromInt model.face
-
-
-type alias Board =
-    { n : Maybe Int
-    , s : Maybe Int
-    , e : Maybe Int
-    , w : Maybe Int
-    , sr : Maybe Int
-    , playArea : Dict ( Int, Int ) Int
-    }
 
 
 type alias Model =
